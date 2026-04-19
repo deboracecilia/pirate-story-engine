7bc32ea902284558fed34de268ea1437555dd5e4
deboracecilia <dede.cecy2002@gmail.com>
2026-04-19 16:17:08 -0300

    feat: criando jogo pirata

---
+++ atividade_hist_2.py
@@ -0,0 +1,133 @@
+#História (conto) - atividade 25/02
+
+# Variáveis da história
+local = "Marés Sussurrantes"
+vilao = "Barba negra"
+
+#Tela de introdução para o usuário
+
+
+
+
+#Usuário (usando input)- atividade 02/03
+nome = input ("Digite o nome do seu heroi: ")
+print (nome)
+poder_do_heroi = input ("Digite o nome do seu ataque supremo: ")
+print (poder_do_heroi)
+nome_navio = input ("Escolha o nome do seu navio: ")
+print (nome_navio)
+frase= input ("Escolha uma frase de efeito antes de atacar: ")
+print (frase)
+acao_heroi = input ("Escolha um poder recém-descoberto: ")
+print (acao_heroi)
+
+# História do conto primeira parte 
+conto1 = "Nas vastas águas, onde o vento carregava segredos antigos e o horizonte parecia infinito, começava a jornada de " + nome +  " que navegava pelos mares no seu grande e velho amigo: " + nome_navio + ", em busca do lendário tesouro One Piece. " + "Ao avistar a misteriosa  " + local + ", você percebe algo estranho no ar. Um riso sombrio ecoa pela ilha... É o temido " + vilao + ":  — Zehahahaha! Então você acha que pode conquistar os mares? Ele ataca primeiro, usando sua escuridão para tentar te puxar para um vórtice destrutivo."
+print (conto1)
+   
+#Decisão do usuário1
+reacao = input ("Digite seu primeiro golpe imediato: ")
+print (reacao)
+
+#História do conto segunda parte
+conto2= "A batalha se intensifica com seu primeiro golpe, " + reacao + ", de forma imediata o chão se parte. Ondas gigantes se chocam contra os penhascos. Mas não foi o suficiente para combater o inimigo, porém você percebe uma fraqueza..." 
+print (conto2)
+
+#Decisão do usuário2
+fraqueza= input("Qual fraqueza que você descobre?")
+print (fraqueza)
+
+conto3= "Descobrindo que a fraqueza é " + fraqueza + ". Você lança seu poder supremo: " + poder_do_heroi + ". Porém, não foi o suficiente para derrubá-lo. Com a raiva subindo para a cabeça seu último movimento, é seu poder recém descoberto, " + acao_heroi + ", ao usar, você grita: " + frase + ". Durante o seu grito, você repara que de forma imediata a ilha treme o bastante deixando o " + vilao + " DERROTADO! Após, a vitória o vento sopra novamente em direção à Grand Line. O One Piece ainda espera por você." 
+print (conto3)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Hitória (conto) - atividade UC2
+
+print ("Depois da queda de" + vilao + ", a ilha das " + local + "mergulhou em um silêncio profundo. O mar, que antes rugia violentamente, agora apenas balançava calmamente como se respeitasse a batalha que acabara de acontecer.")
+print ("Você permanece por alguns instantes olhando o horizonte. O vento bate nas velas do seu navio, " + nome_navio + ", como se o chamasse novamente para o destino inevitável. Do chão quebrado da " + local + ",alguns pedaços de pedra começam a deslizar para o mar. A batalha foi tão intensa que parte da ilha simplesmente não suportou. Ao observar as pedras deslizarem você nota algo estranho, um mapa de metal. O objeto é antigo, coberto por símbolos que lembram os lendários Poneglyph. Quando você o toca, percebe que não é um mapa comum. Ele aponta para três lugares misteriosos: ")
+print (" A Ilha do Relâmpago Eterno, onde tempestades nunca param")
+print ("E uma última marca… bem no coração da Grand Line")
+print ("No topo do mapa há apenas uma frase gravada: Aquele que derrotar as sombras poderá encontrar o caminho final.")
+print ("Digite qual caminho seguir?")
+print ("Ilha do Relâmpago Eterno")
+print ("Coração da Grand Line")
+
+#Usuário seguiu o primeiro caminho - Ilha do relâmpago eterno
+conto4= ("O céu não apenas escurece… ele parece rasgar. As velas do" + nome_navio + "estalam violentamente enquanto o vento gira em espiral. O mar deixa de ser água, torna-se um campo vivo de energia.  Relâmpagos não caem… eles caçam. A bússola gira tão rápida que se parte ao meio. Algo ali não quer que você siga em frente. De repente. Um clarão branco explode ao lado do navio. O impacto levanta uma muralha de água que quase engole tudo. Quando a visão retorna… Uma rocha flutua acima do mar, cercada por correntes elétricas. E sobre ela… uma figura imóvel. Seus olhos brilham como o próprio céu:")
+print (conto4)
+print (" — Os fracos afundam… os fortes são testados. ")
+print ("O trovão responde à voz dele: ")
+print (" — Diga-me, pirata… você veio sobreviver… ou ser apagado? ")
+print ("Thorion, o Senhor das Tempestades, ergue lentamente a mão. O tempo parece parar por um segundo. Então, o céu inteiro dispara. ")
+print ("⚡⚡⚡⚡⚡")
+print ("Dezenas de relâmpagos descem ao mesmo tempo, mas algo está errado… Eles não são aleatórios. Eles seguem um padrão. Eles estão te estudando. ")
+
+#Batalha utilizando contador para contagem de vidas
+print ("Status inicial")
+print (" _______________________________________________")
+print ("|Sua vida: X | Vida de Thorion: X | Energia : X |")
+print (" ̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅̅")
+
+#Batalha usuário vs vilão 
+print ("Os relâmpagos caem de forma brutal e imprevisível. Você não entende nada ainda... Mas precisa fazer uma escolha")
+print ("Escolha: ")
+print ("1. Observar os relâmpagos")
+print ("2. Atacar diretamente")
+print ("3. Usar poder")
+
+#Resultados das escolhas
+#opção 1
+print ("Dano recebido: ")
+print ("Energia: ")
+#opção 2
+print ("Dano recebido: ")
+print "Energia: ")
+#opção 3
+print ("Dano recebido: ")
+print ("Energia: ")
+
+#Continuação da batalha
+print ("A tempestade piora. Os relâmpagos começam a mirar você.)
+print ("Escolha: ")
+print (" 1. Continuar observando novamente")
+print (" 2. Atacar ")
+print (" 3. Poder ")
+
+#Resultado das escolhas
+#opção 1
+print ("Dano recebido: ")
+print ("Energia: ")
+#opção 2
+print ("Dano recebido: ")
+print ("Energia: ")
+#opçao 3
+print ("Dano recebido: ")
+print ("Energia: ")
+
+#Decisão final da batalha
+print ("Thorion se irrita:  ")
+print ("  — Você não deveria estar vivo ainda… ")
+print (" Thorion levanta as duas mãos. O céu inteiro fica branco: ")
+print (" — DESAPAREÇA. ")
+
+#Decisão final do usuário para essa batalha
+print (" 1. Ataque final")
+print (" 2. Defender")
+print (" 3. Poder")
+
+#Usuário seguiu o segundo caminho - Coração da Grand Line
+print (" O clima muda completamente. O mar fica calmo... silencioso demais. De repente, o navio para.")
+print (" Uma figura aparece no convés. Um homem encapuzado" )
+print (" — Você chegou longe demais...")
+print ("Ele levanta o rosto... ")
+print (" É uma versão sua.")
+print (" — Eu sou você... que já encontrou o One Piece.")
+
+#opção de escolher armas para lutar
+
+       
+
+
+
+
+                    
